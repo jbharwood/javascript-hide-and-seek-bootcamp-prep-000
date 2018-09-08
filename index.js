0cbdd5346a1a8036bb8c7fd465a50791e973fb7d
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-08 20:16:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector(`#nested .target`)
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll(`.ranked-list`)
+  for (var i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i]) + n
+  }
+}
+
+function deepestChild() {
+  var node = document.querySelector(`div#grand-node`)
+  var nextNode = node.children[0]
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  } 
+  return node
+}
