aaa4cfc70308d5e132f0fe78324e7389e143dc96
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-08 20:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector(`#nested .target`)
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll(`.ranked-list`)
+  for (var i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i]) + n
+  }
+}
+
+function deepestChild() {
+  var grandNode = document.querySelectorAll(`div#grand-node`)
+  console.log(`grandNode: ` + grandNode)
+  for (var i = 0; i < grandNode.length; i++) {
+        grandNode.querySelectorAll(`div`)
+        console.log(`loop: ` + grandNode.querySelectorAll(`div`))
+  }
+}
