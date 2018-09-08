dd4ac184701579156b7f3c0be77ec803e4d06e2f
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-08 20:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,8 @@ function increaseRankBy(n) {
 function deepestChild() {
   var node = document.querySelectorAll(`div#grand-node`)
   var nextNode = node.children[0]
-  
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  } return node
 }
